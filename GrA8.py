80f0bddf2fb04618d581f36116feacfa9dd73f0e
swastik-omm <84169890+swastik-omm@users.noreply.github.com>
2023-04-19 11:42:19 +0530

    Add files via upload

--- GrA8.py
+++ GrA8.py
@@ -10,11 +10,11 @@ st.write('The first number is ', c)
 largest = 0
 
 if st.button('Find the Largest one'):
-    if a > b and a > c:
+    if a >= b and a >= c:
         largest = a
-    if b > a and b > c:
+    if b >= a and b >= c:
         largest = b
-    if c > a and c > b:
+    if c >= a and c >= b:
         largest = c
     st.write(largest, "is the largest of three numbers.")
 else:
